cd98c7230912c8b2efbcda0d2afefc3285326045
myangelxe <MyMyali13@icloud.com>
2026-05-31 16:22:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    document.getElementById("message").style.display = "block";
+}
+
+function activateTartaglia() {
+    document.getElementById("tartagliaOverlay").style.display = "flex";
+}
+
+function closeTartaglia() {
+    document.getElementById("tartagliaOverlay").style.display = "none";
+}
